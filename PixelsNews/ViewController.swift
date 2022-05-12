6a9b230e10c0d639a8bfa851f6da8b8118e6f9f7
Berkay Sancar <sancarberkay@gmail.com>
2022-05-12 21:53:32 +0300

    Initial Commit

---
+++ PixelsNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PixelsNews
+//
+//  Created by Berkay Sancar on 12.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
